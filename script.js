97e3d0cf83ebbcf848ca0b31f8a94e42711baf91
kareena <angewills387@gmail.com>
2023-11-18 23:44:29 +0545

    funtion edited

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 let userInput = document.getElementById("date");
 userInput.max = new Date().toISOString().split("T")[0];
+let result = document.getElementById("result");
+
 function calculateAge() {
     let birthDate = new Date(userInput.value);
     let d1 = birthDate.getDate();// store date
@@ -9,9 +11,9 @@ function calculateAge() {
 
     let today = new Date();
 
-    let d2 = birthDate.getDate();
-    let m2 = birthDate.getMonth() + 1;
-    let y2 = birthDate.getFullYear();
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
 
     let d3, m3, y3;
 
@@ -36,7 +38,7 @@ function calculateAge() {
 
     }
 
-    console.log(y3, m3, d3);
+     result.innerHTML = `You are ${y3} years, ${m3} months and ${d3} days old`;
 }
 function getDaysInMonth(year, month) {
     return new Date(year, month, 0).getDate();
